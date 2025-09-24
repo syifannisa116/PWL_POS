fb04829709fcbd0518a5be159b4313d16e59f5d1
syifaannisa116 <syifaannisagnm06@gmail.com>
2025-09-24 08:57:52 +0700

    Prak 2 kelar

--- database/migrations/2025_09_24_015414_create_m_kategori_table.php
+++ database/migrations/2025_09_24_015414_create_m_kategori_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('m_level', function (Blueprint $table) {
-            $table->id('level_id');
-            $table->string('level_kode', 10)->unique();
-            $table->string('level_nama', 100);
+        Schema::create('m_kategori', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('m_level');
+        Schema::dropIfExists('m_kategori');
     }
 };

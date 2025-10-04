5a458f3af3f42b8898bd097e4d2544ee0cb2f7f8
syifaannisa116 <syifaannisagnm06@gmail.com>
2025-10-04 19:10:06 +0700

    Prak 2.4 j4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,13 +5,19 @@
 </head>
 <body>
     <h1>Data User</h1>
-    <table border="1" cellpadding="5" cellspacing="0">
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <td><b>Jumlah Pengguna</b></td>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
         </tr>
         <tr>
-            <td>{{ $jumlah }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
-    </table>
+     </table>
 </body>
 </html>

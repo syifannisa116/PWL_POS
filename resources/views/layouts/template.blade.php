6760f8bbb0da42325373c1143354d31892de37e0
syifaannisa116 <syifaannisagnm06@gmail.com>
2025-10-22 22:47:10 +0700

    Prak 2 j6

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -68,11 +68,11 @@
   <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
 
   <!--jquery-validation-->
-  <script src="{{ asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script> 
-  <script src="{{ asset('plugins/jquery-validation/additional-methods.min.js') }}"></script>
+  <script src="{{ asset('adminlte/plugins/jquery-validation/jquery.validate.min.js') }}"></script> 
+  <script src="{{ asset('adminlte/plugins/jquery-validation/additional-methods.min.js') }}"></script>
 
   <!-- SweetAlert2 -->
-  <script src="{{ asset('plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+  <script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
   
   <!-- AdminLTE -->
   <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}', [UserController::class, 'show']);
     Route::get('/{id}/edit', [UserController::class, 'edit']);
     Route::put('/{id}', [UserController::class, 'update']);
+
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+    Route::get('/{id}/update_ajax', [UserController::class, 'update_ajax']);
+
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
